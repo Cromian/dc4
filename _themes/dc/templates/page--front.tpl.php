86b268cc1c34451d9707f2bf938a7513ef98d288
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2021-03-17 20:38:37 -0400

    intial commit

---
+++ _themes/dc/templates/page--front.tpl.php
@@ -0,0 +1,76 @@
+<section class="subheader intro-banner">
+  <div class="container">
+    <div class="banner-text">
+      <h1><span class="subtitle">Creative</span> <span class="lb">High-Quality</span> Web Development.</h1>
+      <p>Digital Confection is a creative web development studio specializing in building simple, elegant, and innovative websites. From exceptional customer services, timely assistance, and creative thinking, we help customers succeed efficiently and effectively.</p>
+      <a href="mailto:hello@digitalconfection.com?Subject=Hello%20From%20Website" class="button">Lets Connect</a>
+    </div>
+  </div>
+</section>
+
+<section id="services" aria-label="services">
+  <div class="doodle-lightbulb"></div>
+  <div class="container">
+    <h2 class="arrow-text"><span>Services</span></h2>
+    <div class="three-col">
+      <div>
+        <h3><span class="subtitle">Assorted</span> Development.</h3>
+        <p>Our broad offering of technical services allows us to be a one-stop web development shop. From custom WordPress and Drupal websites to Shopify, e-commerce, mobile, and Squarespace, our team can handle just about any web development task.</p>
+      </div>
+      <div>
+        <h3><span class="subtitle">Innoivative</span> Product Stratagy.</h3>
+        <p>Technology is only as good as the strategy behind it. With well over 15 years of multi-disciplinary experience, we are experts in helping customers truly understand what they want and how they can effectively achieve it.</p>
+      </div>
+      <div>
+        <h3><span class="subtitle">Memorable</span> Graphic Design.</h3>
+        <p>From logos, templates, mobile mockups, and wireframes, our diverse skillset allows us to easily complement our other services and help you define a style that you will enjoy.</p>
+      </div>
+    </div>
+  </div>
+  <div class="doodle-scribble"></div>
+</section>
+
+<section id="about" aria-label="about" class="alt-section">
+  <div class="container">
+    <h2 class="arrow-text"><span class="alt">Satisfaction</span></h2>
+    <div class="two-col">
+      <div>
+        <h3><span class="subtitle">We Really</span> Enjoy What We Do.</h3>
+        <p>Having worked on various projects, from large government websites to individual small business e-commerce shops, our team understands what it means to build a good product.</p><p>We know what questions to ask and what services to utilize, giving you peace of mind that we will do your job with speed and efficiency. When working with Digital Confection, here is what you will receive:</p>
+        <ul>
+          <li>Friendly and proactive customer service</li>
+          <li>Handcrafted code without the bloat</li>
+          <li>Timely delivery and deployment</li>
+          <li>Straightforward pricing</li>
+          <li>Piece of mind</li>
+        </ul>
+      </div>
+      <div>
+        <div class="doodle-office"></div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="testimonials" aria-label="testimonials"> 
+  <div class="container">
+    <h2 class="arrow-text"><span>Testimonials</span></h2>
+    <div class="three-col">
+      <div>
+        <div class="happy-quote">
+          <strong>Awesome & Reliable</strong>
+          <p>"Working with Misha has been awesome. He is reliable, communicative, innovative and completes tasks in a timely manner. We look forward to a continued successful working relationship. Thanks for all of your hard work!"</p>
+          <div class="credit">- Tinesi B. Minikon, TBM Graphix, LLC</div>
+        </div>
+      </div>
+      <div></div>
+      <div>
+        <div class="happy-quote">
+          <strong>Professional & Efficient</strong>
+          <p>"We have worked with Misha on multiple high-profile Federal projects over the past <?php echo date('Y') - '2016'; ?> years. He has brought a lot of value to how we develop custom websites for our clients, while maintaining a professional and efficient work ethic. We look forward to continuing our relationship for many years to come!"</p>
+          <div class="credit">- Daniel Bowens, Async-Nu Microsystems</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ _themes/dc/templates/page.tpl.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title><?php render_siteTitle($page_data); ?></title>
+  <meta name="description" content="<?php render_siteDescription($page_data); ?>">
+  <?php render_themeCSS(); ?>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,700;1,400&family=Architects+Daughter&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+
+  <?php render_templateMetaHead($page_data); ?>
+</head>
+<body>
+
+<?php #  Renders the site banner. ?>
+<?php render_siteBanner()?>
+
+<header>
+  <div class="container nav-well">
+    <div>
+      <a href="<?php render_baseUrl(); ?>" class="logo" aria-label="Logo"></a>
+    </div>
+    <div>
+      <!-- <div id="menu-icon" onclick="slideToggle()">☰</div> -->
+      <nav id="menu-well"><?php render_siteMenu(); ?></nav>
+    </div>
+  </div>
+</header>
+
+<main>
+  <!-- This line renders a page overide i.e. page--front.tpl.php -->
+  <?php if (isset($page_content)) { include($page_content); } ?>
+
+  <div class="container">
+    <!-- This line renders the content template, i.e. a post or page. -->
+    <?php if (isset($page_data['template_type']) && $page_data['template_type'] == 'content') { ?>
+      <?php render_templateContent($page_data); ?>
+    <?php } ?>
+
+    <!-- This line renders a list page, i.e. blog, tag or category -->
+    <?php if (isset($page_data['template_type']) && $page_data['template_type'] == 'list') { ?>
+      <?php  render_templateList($page_data); ?>
+    <?php } ?>
+
+    <!-- This renders the 404 page. Customize as you see fit. -->
+    <?php if (isset($page_data['status'])) { if ($page_data['status'] == '404') {?>
+      <h1>Gratz. You Broke it!</h1>
+      <p>It looks like something terrible has happened, and this page no longer exists.</p>
+    <?php } } ?>
+  </div>
+</main>
+
+<footer class="alt-section" id="contact">
+  <div class="container">
+    <div class="two-col">
+      <div>
+        <div class="banner-text">
+          <h2><span class="subtitle">How Can</span> We Help You?</h2>
+          <p>Our team has exceptional proficiency in building one-of-a-kind websites, leveraging the latest web technologies, design principles, and product strategies. Let us know how we can do the same for you!</p>
+          <a href="mailto:hello@digitalconfection.com?Subject=Hello%20From%20Website" class="button">Lets Connect</a>
+        </div>
+      </div>
+      <div>
+        <div class="doodle-me"></div>
+      </div>
+    </div>
+    <div class="msg center">Have a fabulous <?php echo date('l'); ?>!</div>
+    <div class="cc center">
+      ©<?php echo date('Y') . ' '?><a href="<?php render_baseUrl(); ?>">Digital Confection, LLC</a>.
+    </div>
+  </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php render_themeJS(); ?>
+</body>
+</html>

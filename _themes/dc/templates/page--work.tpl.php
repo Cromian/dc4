3684f0c05e8f1cdbe313b313c9c09dda19bd35ea
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2021-03-27 22:58:46 -0400

    updated code

--- _themes/dc/templates/page--work.tpl.php
+++ _themes/dc/templates/page--work.tpl.php
@@ -3,11 +3,19 @@
     <div class="two-col">
       <div>
         <h1>Our Work</h1>
-        <p>We take pride in our work. When we take on a new project, we embrace it as ours and strive to deliver the perfection it deserves.</p>
+        <p>We take pride in what we do. When we take on a new project, we embrace it as ours and strive to deliver the perfection it deserves. The work we do reflects our values, constantly improving and producing the best.</p>
       </div>
       <div>
-        <!-- <div class="doodle-confection"></div> -->
+        <div class="doodle-globe"></div>
       </div>
     </div>
+  </div>
+</section>
+
+<section class="page-body">
+  <div class="container">
+    <h2 class="arrow-text"><span>How we help</span></h2>
+    <?php render_templateList($page_data, 'no-title'); ?>
+    <div class="doodle-scribble"></div>
   </div>
 </section>

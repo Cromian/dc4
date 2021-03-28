0aafb7f71859cc3153826ad0a5407c5b58308219
Misha Vinokur <misha@Mishas-MacBook-Pro.local>
2021-03-27 23:28:39 -0400

    added work type

---
+++ _themes/dc/templates/page--type--work.tpl.php
@@ -0,0 +1,15 @@
+<section class="subheader intro-banner content">
+  <div class="container">
+    <h1 class="title"><?php echo $page_data['title']; ?></h1>
+  </div>
+</section>
+
+<section class="page-body">
+  <div class="container">
+  <h2 class="arrow-text"><span>Our Process</span></h2>
+    <div class="content-well">
+      <?php echo $page_data['body']; ?>
+    </div>
+    <div class="doodle-scribble"></div>
+  </div>
+</section>
